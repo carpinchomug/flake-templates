9234d77fac5d5dc5ae107c0e02328cf7b52a7e75
Akiyoshi Suda <aki.suda@protonmail.com>
2023-04-29 00:17:43 +0900

    format

--- python-impure/flake.nix
+++ python-impure/flake.nix
@@ -2,37 +2,37 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }:
-  let
-    supportedSystems = [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-  in
-  {
-    devShells = forAllSystems (system: with pkgs.${system}; 
     let
-      libraries = [
-        stdenv.cc.cc.lib
-
-        # For numpy
-        # zlib
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
       ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
-      default = mkShell {
-        venvDir = "./.venv";
+      devShells = forAllSystems (system: with pkgs.${system};
+        let
+          libraries = [
+            stdenv.cc.cc.lib
 
-        packages = [
-          python3Packages.python
-          python3Packages.venvShellHook
-        ];
+            # For numpy
+            # zlib
+          ];
+        in
+        {
+          default = mkShell {
+            venvDir = "./.venv";
 
-        LD_LIBRARY_PATH = "${lib.makeLibraryPath libraries}";
-      };
-    });
-  };
+            packages = [
+              python3Packages.python
+              python3Packages.venvShellHook
+            ];
+
+            LD_LIBRARY_PATH = "${lib.makeLibraryPath libraries}";
+          };
+        });
+    };
 }
