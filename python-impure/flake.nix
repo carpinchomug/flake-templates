7abde16bbe853a38cdbf262f9a5f2d1f4ceabc29
Akiyoshi Suda <aki.suda@protonmail.com>
2023-04-29 00:01:30 +0900

    fix template for python

--- python-impure/flake.nix
+++ python-impure/flake.nix
@@ -13,43 +13,25 @@
     pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
   in
   {
-    devShells = forAllSystems (system: with pkgs.${system}; {
+    devShells = forAllSystems (system: with pkgs.${system}; 
+    let
+      libraries = [
+        stdenv.cc.cc.lib
+
+        # For numpy
+        # zlib
+      ];
+    in
+    {
       default = mkShell {
         venvDir = "./.venv";
 
-        packages = with pkgs.${system}; [
-          # A Python interpreter including the 'venv' module is required to
-          # bootstrap the environment.
+        packages = [
           python3Packages.python
-
-          # This executes some shell code to initialize a venv in $venvDir
-          # before dropping into the shell
           python3Packages.venvShellHook
-
-          # Those are dependencies that we would like to use from nixpkgs,
-          # which will add them to PYTHONPATH and thus make them accessible
-          # from within the venv.
-          python3Packages.python-lsp-server
-          python3Packages.python-lsp-server.optional-dependencies.all
-          python3Packages.pyls-isort
-          python3Packages.python-lsp-black
-
-          # Add more packages below as needed.
         ];
 
-        # Run this command, only after creating the virtual environment.
-        postVenvCreation = ''
-          unset SOURCE_DATE_EPOCH
-          if [ -f requirements.txt ]; then
-            pip install -r requirements.txt
-          fi
-        '';
-
-        # Now we can execute any commands within the virtual environment.
-        # This is optional and can be left out to run pip manually.
-        postShellHook = ''
-          unset SOURCE_DATE_EPOCH
-        '';
+        LD_LIBRARY_PATH = "${lib.makeLibraryPath libraries}";
       };
     });
   };
