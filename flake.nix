ff294841ffeddd4a7bccb7b05b9bf484825dd90d
Akiyoshi Suda <aki.suda@protonmail.com>
2023-09-20 01:24:59 +0900

    Remove unused templates

--- flake.nix
+++ flake.nix
@@ -2,16 +2,6 @@
   description = "A collection of flake templates";
 
   outputs = { self }: {
-    templates = {
-      skeleton = {
-        path = ./skeleton;
-        description = "minimal template for making a nix shell environment";
-      };
-
-      python-impure = {
-        path = ./python-impure;
-        description = "manage python environment with nix + pip";
-      };
-    };
+    templates = { };
   };
 }
