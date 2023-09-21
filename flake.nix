8e68ecd90aa5193bd72ae0e0633d8ab53ac4f760
Akiyoshi Suda <aki.suda@protonmail.com>
2023-09-21 23:52:44 +0900

    Add python template

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
         path = ./clang;
         description = "C/C++ template that includes a development shell for a clang-based project."
       };
+
+      python = {
+        path = ./python;
+        description = "Python environment for data science";
+      };
     };
   };
 }
